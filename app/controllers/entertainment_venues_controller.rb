0ee6535fec69e63d2fec16e63b912fda6f7c550e
Iliya Svetlichnii <batya2095@gmail.com>
2016-12-17 12:23:37 +0600

    вывел все заведения и добавил возможность просмотра каждого

--- app/controllers/entertainment_venues_controller.rb
+++ app/controllers/entertainment_venues_controller.rb
@@ -1,8 +1,10 @@
 class EntertainmentVenuesController < ApplicationController
   def index
+  	@entertainment_venues = EntertainmentVenue.all
   end
 
   def show
+  	@entertainment_venue = EntertainmentVenue.find(params[:id])
   end
 
   def new
